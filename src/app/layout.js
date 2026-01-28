9b8de2b44fa3c45f04be7b06fef6b3384f165115
mfederico-web <mfederico@scommettendo.it>
2026-01-28 23:15:55 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Generatore Report ADM | Rilevazioni Gioco Fisico',
+  title: 'Generatore Report ADM Livelli di Servizio | Rilevazioni Gioco Fisico',
   description: 'Genera automaticamente i report PDF per le rilevazioni sul Gioco Fisico ai fini del controllo dei Livelli di Servizio ADM',
   keywords: 'ADM, report, gioco fisico, scommesse, PDF generator',
 }
